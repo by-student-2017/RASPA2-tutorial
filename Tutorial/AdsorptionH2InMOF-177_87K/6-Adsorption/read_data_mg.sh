a9e016bcefa13481af2b06f2f4d1b1d9aa4f81c1
by-student-2017 <studentsctest@gmail.com>
2022-04-11 14:21:12 +0900

    windows10

--- Tutorial/AdsorptionH2InMOF-177_87K/6-Adsorption/read_data_mg.sh
+++ Tutorial/AdsorptionH2InMOF-177_87K/6-Adsorption/read_data_mg.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+P0=101325
+
 #grep "Average loading absolute" output*
 #grep "Average loading excess" output*
 #grep "cm^3 (STP)/gr framework" output*
@@ -15,8 +17,8 @@ do
   #echo ${Pa}
   #awk '{if($3=="absolute" && $5=="(STP)/gr"){printf "%d", NR}}' $f
   #awk '{if($3=="excess" && $5=="(STP)/gr"){printf "%d", NR}}' $f
-  nra=`awk -v pre=${Pa} '{if($3=="absolute" && $5=="(STP)/gr"){printf "%d", NR}}' $f`
-  nre=`awk -v pre=${Pa} '{if($3=="excess" && $5=="(STP)/gr"){printf "%d", NR}}' $f`
-  awk -v pre=${Pa} -v NRA=${nra} '{if(NR==(NRA-1)){printf "%12.10f, %12.6f, %12.10f, %4.2f \n", (pre/101325), $6, pre, $6/10}}' $f >> absolute_results_mg.txt
-  awk -v pre=${Pa} -v NRE=${nre} '{if(NR==(NRE-1)){printf "%12.10f, %12.6f, %12.10f, %4.2f \n", (pre/101325), $6, pre, $6/10}}' $f >> excess_results_mg.txt
+  nra=`awk '{if($3=="absolute" && $5=="(STP)/gr"){printf "%d", NR}}' $f`
+  nre=`awk '{if($3=="excess" && $5=="(STP)/gr"){printf "%d", NR}}' $f`
+  awk -v pre=${Pa} -v p0=${P0} -v NRA=${nra} '{if(NR==(NRA-1)){printf "%12.10f, %12.6f, %12.6f, %4.2f \n", (pre/p0), $6, pre, ($6/10)}}' $f >> absolute_results_mg.txt
+  awk -v pre=${Pa} -v p0=${P0} -v NRE=${nre} '{if(NR==(NRE-1)){printf "%12.10f, %12.6f, %12.6f, %4.2f \n", (pre/p0), $6, pre, ($6/10)}}' $f >> excess_results_mg.txt
 done
