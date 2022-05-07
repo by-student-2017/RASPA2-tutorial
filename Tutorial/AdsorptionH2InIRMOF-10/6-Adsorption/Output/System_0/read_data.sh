5e224bbd991879abae1f0d315345bf069dec55c4
by-student-2017 <studentsctest@gmail.com>
2022-05-07 22:31:14 +0900

    ubuntu 20.04 LTS

---
+++ Tutorial/AdsorptionH2InIRMOF-10/6-Adsorption/Output/System_0/read_data.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+P0=101325
+
+#grep "Average loading absolute" output*
+#grep "Average loading excess" output*
+#grep "cm^3 (STP)/gr framework" output*
+
+echo "P/P0, absolute[cm3 (STP)/gr framework], P[Pa]" > absolute_results.txt
+echo "P/P0, excess[cm3 (STP)/gr framework], P[Pa]" > excess_results.txt
+for f in *.data
+do
+  #echo $f
+  f2=${f%.data}
+  fbase=${f2%_*}
+  Pa=${f2//"${fbase}_"/}
+  #echo ${Pa}
+  awk -v pre=${Pa} -v p0=${P0} '{if($3=="absolute" && $5=="(STP)/gr"){printf "%12.10f, %12.6f, %12.6f \n", (pre/p0), $7, pre, p0}}' $f >> absolute_results.txt
+  awk -v pre=${Pa} -v p0=${P0} '{if($3=="excess" && $5=="(STP)/gr"){printf "%12.10f, %12.6f, %12.6f \n", (pre/p0), $7, pre, p0}}'   $f >> excess_results.txt
+done
